71b2000dca37a3e84de04023690d576456518431
Тимур Токтошев <timurtoktosev@MacBook-Air-Timur.local>
2022-02-28 15:01:33 +0600

    data added

---
+++ src/components/data.js
@@ -0,0 +1,24 @@
+import product1 from 'images/products-01.jpg';
+import product2 from 'images/products-02.jpg';
+import product3 from 'images/products-03.jpg';
+
+export const data = [
+    {
+        image: product1,
+        titleTop: 'Blended to Perfection',
+        titleBottom: 'Coffees & Teas',
+        text: 'We take pride in our work, and it shows. Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it\'s our world famous Venezuelan Cappuccino, a refreshing iced herbal tea, or something as simple as a cup of speciality sourced black coffee, you will be coming back for more.'
+    },
+    {
+        image: product2,
+        titleTop: 'Delicious Treats, Good Eats',
+        titleBottom: 'Bakery & Kitchen',
+        text: 'Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods.'
+    },
+    {
+        image: product3,
+        titleTop: 'From Around the World',
+        titleBottom: 'Bulk Speciality Blends',
+        text: 'Travelling the world for the very best quality coffee is something take pride in. When you visit us, you\'ll always find new blends from around the world, mainly from regions in Central and South America. We sell our blends in smaller to large bulk quantities. Please visit us in person for more details.'
+    }
+];
